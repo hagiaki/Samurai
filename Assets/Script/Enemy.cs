79da46ee799694679ad7bf9611fd996ee5d44f33
Hagiaki <hagiaki.favorite@gmail.com>
2021-12-04 20:55:42 +0900

    名前変更(Status→BaseCharacter)

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : Status
+public class Enemy : BaseCharacter
 {
     
 
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : Status//MonoBehavior‚Ì’†g‚ğŒp³
+public class Player : BaseCharacter//MonoBehavior‚Ì’†g‚ğŒp³
 {
     /*public float speed = 5.0f;
     public float bulletspeed;
